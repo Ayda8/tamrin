b376a2f889cb3622d15cdceb9f49036e1843dd9d
Ayda8 <aydakabiri71@gmail.com>
2024-10-27 16:51:23 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    static void main(String[] args) {
+        int Limit = 100; // the maximum number we want to9 print the prime numbers of
+        System.out.println("prim numbers up to"+ Limit + ":");
+        for ()
+    }
+}
